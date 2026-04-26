6b103d8aeade9316624187806dfb098c68337422
CHANCENY <nyasuluchance6@gmail.com>
2026-04-27 00:32:33 +0530

    kk

--- pindrop/Templating/TwigEngine.php
+++ pindrop/Templating/TwigEngine.php
@@ -159,7 +159,7 @@ class TwigEngine
         $this->twig->addGlobal('app_version', $this->envProvider->get('APP_VERSION', '1.0.0'));
         $this->twig->addGlobal('app_env', $this->envProvider->get('APP_ENV', 'development'));
         $this->twig->addGlobal('request', Request::createFromGlobals());
-        $this->twig->addGlobal('is_login', $this->container->get('current_user')->isLoggedIn());
+        $this->twig->addGlobal('is_login', $this->container->get('current_user')?->isLoggedIn());
         $this->twig->addGlobal('sidebar_off', (bool) $this->envProvider->get('SIDEBAR_OFF', false));
 
         $this->twig->addFunction(new TwigFunction("supportLanguages", function (){
